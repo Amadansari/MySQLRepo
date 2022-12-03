ba3e523acd4a2d29fe2fe5451fd59da88a75d02a
Amadansari <108404152+Amadansari@users.noreply.github.com>
2022-12-03 15:33:17 +0800

    Test_MySQL_file
    Test MySQL file

---
+++ TestMySQL.sql
@@ -0,0 +1,17 @@
+DROP FUNCTION work_id;
+DELIMITER //
+CREATE FUNCTION work_id(startid INT) RETURNS INT DETERMINISTIC	
+BEGIN
+ DECLARE newid INT;
+  SELECT 345654 into newid;
+  RETURN (newid)-(startid);
+END 
+
+//
+DELIMITER ;
+
+
+Select current_date();
+SELECT * 
+from testdb.eeee;
+Select work_id(_id) as 'id_subtraction' from testdb.eeee;
